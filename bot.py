ed9fde99dcca6cf746d08aa9627e50dbc51c00b8
Benson Yan <bensonyan778@hotmail.com>
2024-06-07 23:42:48 -0400

    changes

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ import os
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 # Setup logging
 logger = logging.getLogger('discord')
@@ -28,6 +29,7 @@ logger.addHandler(handler)
 # Create intents
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True # lets bot see all members
 
 # Create the client
 client = discord.Client(intents=intents)
@@ -35,15 +37,25 @@ client = discord.Client(intents=intents)
 # Define events
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    guild = discord.utils.get(client.guilds, name=GUILD) # from all guilds, take the name of guild we have as GUILD
+    print(
+        f'{client.user} is connected to the following guild:\n' # clients name
+        f'{guild.name}(id: {guild.id})' # guilds name with ID
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
 
 @client.event
 async def on_message(message):
+
     if message.author == client.user:
         return
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+
+
 
 # Run the client, suppressing the default logging configuration
 client.run(token, log_handler=None)
